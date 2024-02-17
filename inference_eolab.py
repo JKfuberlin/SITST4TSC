90af4b3d6b43d8d1e0c3a4d11fe1a6f5623fe498
JKfuberlin <jonathan.koeltzow@fu-berlin.de>
2024-02-17 08:49:50 +0100

    added gitignore and somehow messed up to upload the wrong inference_eolab.py file...

--- inference_eolab.py
+++ inference_eolab.py
@@ -1,99 +1,170 @@
-import torch
-from torch import nn, Tensor
-from torch.nn.modules.normalization import LayerNorm
-from torch.nn import TransformerEncoder, TransformerEncoderLayer
-import math
+import torch.utils.data as Data # For dataloader class
+import os # for general operating system functionality
+import glob # for retrieving files using strings/regex
+import re # for regex
+import rasterio # for reading rasters
+import geopandas as gpd # for reading shapefiles
+import numpy as np
+import datetime # for benchmarking
+import torch # for loading the model and actual inference
+import rioxarray as rxr # for raster clipping
+import multiprocessing # for parallelization
+from shapely.geometry import mapping # for clipping
+from rasterio.transform import from_origin # for assigning an origin to the created map
+import pandas as pd
 
-'''
-This script defines an instance of the Transformer Object for Classification
-It consists of two classes, one for Positional Encoding and another for the classifier itself
-Both have a __init__ for initialization and a 'forward' method that is called during training and validation 
-because they are both subclasses of nn.Module, and this is a required method in PyTorch's module system.
+def predict(data_for_prediction_loader):
+    model_pkl.eval() # set model to eval mode to avoid dropout layer
+    with torch.no_grad(): # do not track gradients during forward pass to speed up
+        for (inputs) in data_for_prediction_loader:
+            inputs = inputs.to(device)  # put the data in gpu
+            output_probabilities = model_pkl(inputs)  # prediction
+            _, predicted_class = torch.max(output_probabilities,1)  # retrieving the class with the highest probability after softmax
+    return predicted_class
+# the underscore means that the first entry of the torch.max function is discarded and only the second written into predicted
 
-'''
-dropout=0.1
-max_len = 5000 # defining maximum sequence length the model will be able to process here
-# model dimensions (hyperparameter), dropout is already included here to make the model less prone to overfitting due to a specific sequence, max_length is defined. DOY needs to be smaller than that
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-# I don't know if this makes sense here, as device should be assigned in the main script where model is trained / inference happens
+# GPT:
+def predict2(data_for_prediction_loader):
+    model_pkl.eval()  # Set model to eval mode to avoid dropout layer
+    all_predicted_classes = []
+    with torch.no_grad():  # Do not track gradients during forward pass to speed up
+        for inputs in data_for_prediction_loader:
+            inputs = inputs.to(device)  # Put the data in GPU
+            output_probabilities = model_pkl(inputs)  # Prediction
+            _, predicted_class = torch.max(output_probabilities, 1)  # Retrieving the class with the highest probability after softmax
+            all_predicted_classes.append(predicted_class)
+    return torch.cat(all_predicted_classes, dim=0)
 
-class PositionalEncoding(nn.Module):
-    """Inject some information about the relative or absolute position of the tokens in the sequence.
-        The positional encodings have the same dimension as the embeddings, so that the two can be summed.
-        Here, we use sine and cosine functions of different frequencies.
-    .. math:
-        \text{PosEncoder}(pos, 2i) = sin(pos/10000^(2i/d_model))
-        \text{PosEncoder}(pos, 2i+1) = cos(pos/10000^(2i/d_model))
-        \text{where pos is the word position and i is the embed idx)
-    Args:
-        d_model: the embed dim (required).
-        dropout: the dropout value (default=0.1).
-        max_len: the max. length of the incoming sequence (default=5000).
-    Examples:
-        pos_encoder = PositionalEncoding(d_model)
-    """
-    def __init__(self, d_model:int, dropout=0.1, max_len=5000): # model dimensions (hyperparameter), dropout is already included here to make the model less prone to overfitting due to a specific sequence, max_length is defined. DOY needs to be smaller than that
-        super(PositionalEncoding, self).__init__() # The super() builtin returns a proxy object (temporary object of the superclass) that allows us to access methods of the base class.
-        # i do not understand what that means
-        self.dropout = nn.Dropout(p=dropout) # WTF i do not understand, what this does
-        pe = torch.zeros(max_len, d_model) # positional encoding object is initialized with zeros, according to max length and model dimension. 5000 because we need a position on the sin/cos line for every possible DOY
-        positionPE = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1) # used to create a 1-dimensional tensor representing the positions of tokens in a sequence.
-        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model)) # a tensor representing the values used for scaling in the positional encoding calculation
-        # Apply the sinusoidal encoding to even indices and cosine encoding to odd indices
-        pe[:, 0::2] = torch.sin(positionPE * div_term)
-        pe[:, 1::2] = torch.cos(positionPE * div_term)
-        # pe = pe.unsqueeze(0).transpose(0, 1) # torch.Size([5000, 1, 204]) max_len, ?, d_model
-        self.register_buffer('pe', pe) #  Buffers are parameters that are not considered when computing gradients during backpropagation, we do not want to modify the PE
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')  # Device configuration
 
-    def forward(self, doy):
-        doy = doy.to(self.pe.device)
-        return self.pe[doy, :]
+REMOTE = False
 
-    #     # x = x + self.pe[:x.size(0), :] # TODO: this is the problem, here something is added and it's not even DOY but something completely different, probably just the position in the sequence
-    #     # the bigger issue is that is done in the wrong class.
-    #     # TODO: I could just try to pass the pe object to the TransformerClassifier class and concat there
-    #
-    #     return self.dropout(x)
-# TODO: Change this class to match the PE concatenation from pixel_based branch
-class TransformerMultiLabel(nn.Module):
-    def __init__(self, num_bands:int, num_classes:int, d_model:int, nhead:int, num_layers:int, dim_feedforward:int) -> None:
-        super(TransformerMultiLabel, self).__init__()
+if REMOTE == False:
+    print("dall'inizio")
+    model_pkl = torch.load('/point_storage/data/Transformer_1.pkl', map_location=torch.device('cpu'))  # loading the trained model
+    raster_paths = []
+    #tile ="X0066_Y0056"
+    tile ="X0067_Y0058" # Landshut
+    s2dir = glob.glob(('/force/FORCE/C1/L2/ard/' + tile + '/'), recursive=True)  # Initialize path to Sentinel-2 time series
+    raster_paths = []  # create object to be filled with rasters to be stacked
+    tifs = glob.glob(os.path.join(s2dir[0], "*SEN*BOA.tif"))  # i want to ignore the landsat files and stack only sentinel 2 bottom of atmosphere observations
+### get dates
+    dates = [pd.to_datetime(s[35:43], format='%Y%m%d') for s in tifs]
+    dates = pd.to_datetime(dates, format="%Y%m%d").sort_values() # sort ascending
+    comparison_date = pd.to_datetime('20230302', format="%Y%m%d")
+# Filter dates to exclude entries before 20230302
+    dates = dates[dates <= comparison_date]
+# these dates are the DOY that need to be passed to the forward method
+### here, we need to retrieve and assign the correct DOY values
+# get day of the year of startDate
+    t0 = dates[0].timetuple().tm_yday
+    input_dates = np.array([(date - dates[0]).days for date in dates]) + t0
+    seq_len = len(input_dates)
+    doy = np.zeros((seq_len,), dtype=int)
+    DOY = input_dates
+    raster_paths.extend(tifs)  # write the ones i need into my object to stack from
+    years = [int(re.search(r"\d{8}", raster_path).group(0)) for raster_path in raster_paths]  # i need to extract the date from each file path...
+    raster_paths = [raster_path for raster_path, year in zip(raster_paths, years) if year <= 20230302]  # ... to cut off at last image 20230302 as i trained my model with this sequence length, this might change in the future with transformers
+    datacube = [rxr.open_rasterio(raster_path) for raster_path in raster_paths]  # i user rxr because the clip function from rasterio sucks
+    # datacube.append(doy)# the datacube is too large for the RAM of my contingent so i need to subset using the 5x5km tiles
+# grid_tiles = glob.glob(os.path.join('/my_volume/FORCE_tiles_subset_BW/', '*' + tile + '*.gpkg'))
+    grid_tiles = '/point_storage/landshut_minibatch.gpkg'
+# checking workflow:
+    minitile = grid_tiles
+    crop_shape = gpd.read_file(minitile)
+    clipped_datacube = [] # empty object for adding clipped rasters
+    i = 1
+    total = str(len(datacube))
+    for raster in datacube:  # clip the rasters using the geometry
+        crop_shape = gpd.read_file(minitile)
+        print('cropping ' + str(i) + ' out of ' + total)
+        i = i + 1
+        try:
+            clipped_raster = raster.rio.clip(crop_shape.geometry.apply(mapping))
+            clipped_datacube.append(clipped_raster)
+        except:
+            print('not working')
+            break
 
-        self.d_model = d_model
-        # encoder embedding
-        self.src_embd = nn.Linear(num_bands, d_model)
-        # transformer model #duplicating d_model for PE concatenation in second half
-        encoder_layer = TransformerEncoderLayer(d_model*2, nhead, dim_feedforward)
-        encoder_norm = LayerNorm(d_model*2)
-        self.transformer_encoder = TransformerEncoder(encoder_layer, num_layers, encoder_norm)
-        output_size = d_model
-        self.global_max_pooling = nn.AdaptiveMaxPool1d(1) # global max pooling
-        self.fc = nn.Sequential( # this takes the output of the transformer encoder after max pooling and passes it through linear layers to converge on num_classes
-                    nn.Linear(output_size*2, 256),
-                    nn.ReLU(),
-                    nn.Linear(256, num_classes),
-                )
-        # regression
+else:
+    print('running local')
+    # model_pkl = torch.load('/home/j/data/Transformer_1.pkl', map_location=torch.device('cpu'))  # loading the trained model
+    # data_for_prediction = torch.load('/home/j/data/datacube_doy.pt')
+    # clipped_datacube = np.load('/home/j/data/landshut_cropped_dc.npy')
+    # DOY = pandas.read_csv('/home/j/data/doy_pixel_subset.csv', sep = '\t', header = None)
+    DOY = pd.read_csv('/point_storage/data/doy_pixel_subset.csv', sep='\t', header=None)
+    clipped_datacube = np.load('/point_storage/data/landshut_cropped_dc.npy')
+    if torch.cuda.is_available() == False:
+        model_pkl = torch.load('/point_storage/data/Transformer_1.pkl', map_location=torch.device('cpu'))
+    crop_shape = gpd.read_file('/point_storage/landshut_minibatch.gpkg')
 
-    def forward(self, input_sequence:Tensor, num_bands:int, num_classes:int) -> Tensor:
-        if len(input_sequence.shape) == 2:
-            # Add a batch dimension if it's not present (assuming batch size of 1)
-            input_sequence = input_sequence.unsqueeze(1)
-        input_sequence_bands = input_sequence[:,:,0:num_bands]  # this is the input sequence without DOY, shape should be [batch_size, seq_len, num_bands]
-        # input_sequence_bands = input_sequence_bands.permute(0, 2, 1) # else use this to reshape
-        obs_embed = self.src_embd(input_sequence_bands)  # [batch_size, seq_len, d_model] #
-        self.PEinstance = PositionalEncoding(d_model=self.d_model, max_len=max_len)
-        # this is where the input of form [batch_size, seq_len, n_bands] is passed through the linear transformation of the function src_embd()
-        # to create the embeddings
-        # Repeat obs_embed to match the shape [batch_size, seq_len, embedding_dim*2]
-        x = obs_embed.repeat(1, 1, 2)
-        # Add positional encoding based on day-of-year
-        # X dimensions are [batch_size, seq_length, d_model*2], iterates over number of samples in each batch
-        for i in range(input_sequence.size(0)):
-            x[i, :, self.d_model:] = self.PEinstance(input_sequence[i, :, num_bands].long()).squeeze()
-        # each batch's embedding is sliced and the second half replaced with a positional embedding of the DOY (11th column of the input_sequence) at the corresponding observation i
-        output = self.transformer_encoder(x) # output: [seq_len, batch_size, d_model]
-        output = self.global_max_pooling(output.permute(0,2,1)).squeeze(2)
-        output = self.fc(output)  # GPT should be [batch_size, num_classes]
-        # final shape: [batch_size, num_classes]
-        return output
+print('DOY, model and datacube loaded')
+
+DOY = np.array(DOY)
+datacube_np = np.array(clipped_datacube, ndmin = 4) # this is now a clipped datacube for the first minitile, fixing it to be 4 dimensions
+# Reshape doy to have a new axis
+doy_reshaped = DOY.reshape((329, 1, 1, 1))
+
+# Repeat the doy values along the third and fourth dimensions to match datacube_np
+doy_reshaped = np.repeat(doy_reshaped, 500, axis=2)
+doy_reshaped = np.repeat(doy_reshaped, 500, axis=3)
+# Concatenate along the second axis
+datacube_np = np.concatenate((datacube_np, doy_reshaped), axis=1)
+
+# datacube_np.shape
+datacube_torch16 = torch.tensor(datacube_np) # turn the numpy array into a pytorch tensor, the result is in int16..
+datacube_torch32 = datacube_torch16.to(torch.float32) # ...so we need to transfer it to float32 so that the model can use it as input
+# datacube_torch32.shape # torch.Size([320, 10, 500, 500])
+data_for_prediction = datacube_torch32.permute(2, 3, 0, 1)  # rearrange data
+data_for_prediction.shape #torch.Size([500, 500, 320, 10])
+# [500, 500, 329, 10] after DOY concatentation, expected [500, 500, 329, 11]
+'''now i need to turn the datacube data into the DataLoader format to enable prediction'''
+data_for_prediction = data_for_prediction.to(device)
+data_for_prediction_loader = Data.DataLoader(data_for_prediction, batch_size=1, shuffle=True, num_workers=0, drop_last=False)
+
+print('starting for loop prediction')
+x = 500
+y = 500
+import datetime
+result = np.zeros((x, y, 1))
+for row in range(x):
+    print(row)
+    print(datetime.datetime.now())
+    for col in range(y):
+        print(col)
+        print(datetime.datetime.now())
+        for pixel in data_for_prediction_loader:
+            pixel = pixel.to(device)
+            predicted_class = predict2(pixel)
+            result[row, col, :] = predicted_class
+
+# result = torch.tensor(result)  # Convert the list to an array
+map = result
+print('loop done')
+
+crop_geometry = crop_shape.geometry.iloc[0]
+crop_bounds = crop_geometry.bounds
+origin = (crop_bounds[0], crop_bounds[3])  # Extract the origin from the bounds
+
+# construct metadata from minitile and matrix
+metadata = {
+    'driver': 'GTiff',
+    'width': map.shape[1],
+    'height': map.shape[0],
+    'count': 1,  # Number of bands
+    'dtype': map.dtype,
+    'crs': 'EPSG:3035',  # Set the CRS (coordinate reference system) code as needed
+    'transform': from_origin(origin[0], origin[1], 10, 10)  # Set the origin and pixel size (assumes each pixel is 1 unit)
+}
+
+print('writing')
+with rasterio.open(os.path.join('/point_storage/', 'landshut_transformer.tif'), 'w', **metadata) as dst:
+    # dst.write_band(1, map.astype(rasterio.float32))
+    dst.write(map.astype(rasterio.float32), indexes=1)
+print('written')
+
+# map_reshaped = map.transpose(1, 2, 0, 3)
+#
+# with rasterio.open(os.path.join('/point_storage/', 'landshut_transformer.tif'), 'w', **metadata) as dst:
+#     for i in range(map_reshaped.shape[3]):
+#         dst.write_band(i + 1, map_reshaped[:, :, :, i].astype(rasterio.float32))
